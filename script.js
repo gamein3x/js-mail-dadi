4d57f09e3fead1e9f54a67816e63dd10744b2619
Ivan Caldarella <gamein3x@gmail.com>
2026-03-26 16:33:04 +0100

    Eseguita seconda parte dell'esercizio integrandoli in un unico sito web. Nome sito e autore aggiunti nell'html.

--- script.js
+++ script.js
@@ -13,20 +13,30 @@ Non è necessario provvedere alla validazione delle email, però se volete è un
 
 const listaMailAccettate = ['tiziocaio@pipomail.com', 'rossimario@zmail.it', 'carlobianchi@pipomail.com', 'audyezio@zmail.it', 'topopippo@pipomail.com'];
 
-let mailUser = prompt('Inserisci la tua mail');
+let mailUser = prompt('Inserisci la tua mail per giocare.');
 let mailCheck = false;
 
-for (i=0; i<listaMailAccettate.length; i++) {
+for (i = 0; i < listaMailAccettate.length; i++) {
     if (mailUser !== listaMailAccettate[i]) {
         console.log('Non questa, prossima.');
     } else {
         mailCheck = true;
         console.log('mail corretta');
     }
-} 
+}
 
 if (mailCheck === true) {
     alert('Mail corretta, benvenuto.');
+    const dicePlayer = Math.floor(Math.random() * 6) + 1;
+    const diceCPU = Math.floor(Math.random() * 6) + 1;
+
+    if (dicePlayer > diceCPU) {
+        alert(`Dado player ${dicePlayer} > Dado CPU ${diceCPU}. Hai vinto!`)
+    } else if (diceCPU > dicePlayer) {
+        alert(`Dado CPU ${diceCPU} > Dado player ${dicePlayer}. Hai perso.`)
+    } else {
+        alert(`Dado player ${dicePlayer} = Dado CPU ${diceCPU}. Pareggio.`)
+    }
 } else {
     alert('Mail sbagliata. Ricarica e riprova.')
 }
@@ -34,3 +44,6 @@ if (mailCheck === true) {
 /*Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+
+
