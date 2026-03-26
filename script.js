3ad0ec9f5807dd33dada9a45633aa910632f4da3
Ivan Caldarella <gamein3x@gmail.com>
2026-03-27 00:00:32 +0100

    Non so da quanto non pusho, buckshot roulette funziona

--- script.js
+++ script.js
@@ -27,6 +27,10 @@ for (i = 0; i < listaMailAccettate.length; i++) {
 
 if (mailCheck === true) {
     alert('Mail corretta, benvenuto.');
+    /*Gioco dei dadi
+        Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+        Stabilire il vincitore, in base a chi fa il punteggio più alto. 
+    */
     const dicePlayer = Math.floor(Math.random() * 6) + 1;
     const diceCPU = Math.floor(Math.random() * 6) + 1;
 
@@ -39,11 +43,19 @@ if (mailCheck === true) {
     }
 } else {
     alert('Mail sbagliata. Benvenuto al gioco 2');
+    /*Buckshot Roulette
+        Generare un "caricatore" con 2 colpi "live" e 3 colpi "blank" (o casuali da 1 a 3) in posizioni random.
+        Dichiara al giocatore quanti colpi e di che tipo sono nel caricatore.
+        Giocatore scrive "me" se vuole sparare se stesso, "you" se vuole sparare il prossimo.
+        Se spari un blank a te stesso, hai un turno in più - Se spari un blank al CPU, turno prossimo.
+        Se spari un live a te stesso, perdi una vita, turno prossimo. - Se spari un live al CPU, perde una vita, turno prossimo.
+        Una vita ognuno. (Metti comunque win condition vita del CPU a 0 come win condition)
+    */
     let playerLife = 1;
     let cpuLife = 1;
     const liveNum = Math.floor(Math.random() * 3) + 1;
     const blankNum = Math.floor(Math.random() * 3) + 1;
-
+    const totalNumConst = blankNum + liveNum;
 
     let live = liveNum;
     let blank = blankNum;
@@ -53,7 +65,7 @@ if (mailCheck === true) {
 
 
     // Carica fucile
-    while (loader.length < totalNum) {
+    while (loader.length < totalNum && playerLife > 0 && cpuLife > 0) {
         let liveOrBlank = Math.round(Math.random());
         if (liveOrBlank === 1 && liveNum > 0) {
             loader.push(1);
@@ -65,10 +77,16 @@ if (mailCheck === true) {
     }
 
     console.log(loader);
-    console.log(`${blankNum} blank, ${blankNum} live.`);
+    console.log(`${blankNum} blank, ${liveNum} live.`);
 
-    while (loader.length > 0) {
-        const action = prompt(`${blankNum} blank, ${liveNum} live. Scrivi "me" per sparare a te stesso o "you" per sparare al CPU. Buona fortuna.`);
+    while (loader.length > 0 && playerLife > 0 && cpuLife > 0) {
+        let messaggio = "";
+        if (loader.length === totalNumConst) {
+            messaggio = `${blankNum} blank, ${liveNum} live. Scrivi "me" per sparare a te stesso o "you" per sparare al CPU. Buona fortuna.`;
+        } else if (loader.length !== totalNumConst) {
+            messaggio = `Turno tuo. A chi spari?`;
+        }
+        const action = prompt(messaggio);
         if (action === 'me') {
             let shot = loader.shift();
             if (shot === 0) {
@@ -78,37 +96,131 @@ if (mailCheck === true) {
                 alert('Hai preso un live. -1 Vita.');
                 playerLife--;
                 console.log('Live sparato al player ' + loader);
+                if (playerLife > 0) {
+                    let cpuTurn = true;
+                    while (cpuTurn === true) {
+                        const cpuShotDirection = Math.round(Math.random());
+                        if (cpuShotDirection === 0) {
+                            console.log('CPU spara al player');
+                            let cpuShot = loader.shift();
+                            if (cpuShot === 0) {
+                                console.log('Blank, povero idiota.');
+                                alert('CPU ti spara un blank. Mancato.');
+                                cpuTurn = false;
+                            } else {
+                                console.log('Live, player preso');
+                                alert('CPU ti spara un live. Preso.')
+                                playerLife--;
+                                cpuTurn = false;
+                            }
+                        } else {
+                            console.log('CPU si spara');
+                            let cpuShot = loader.shift();
+                            if (cpuShot === 0) {
+                                console.log('Si spara un blank');
+                                alert('CPU si spara un blank. Turno del CPU.');
+                                cpuTurn = true;
+                            } else {
+                                console.log('Si spara un live');
+                                alert('CPU si spara un live. Preso.')
+                                cpuLife--;
+                                cpuTurn = false;
+                            }
+                        }
+                    }
+                }
             }
-        }
-        if (action === 'you') {
+        } else if (action === 'you') {
             let shot = loader.shift();
             if (shot === 0) {
-                alert('Hai sparato un blank.');
+                alert('Hai sparato un blank. Mancato.');
                 console.log('Blank sparato al CPU' + loader);
+                let cpuTurn = true;
+                while (cpuTurn === true && loader.length > 0 && playerLife > 0) {
+                    const cpuShotDirection = Math.round(Math.random());
+                    if (cpuShotDirection === 0) {
+                        console.log('CPU spara al player');
+                        let cpuShot = loader.shift();
+                        if (cpuShot === 0) {
+                            console.log('Blank, povero idiota.');
+                            alert('CPU ti spara un blank. Mancato.');
+                            cpuTurn = false;
+                        } else {
+                            console.log('Live, player preso');
+                            alert('CPU ti spara un live. Preso.')
+                            playerLife--;
+                            cpuTurn = false;
+                        }
+                    } else {
+                        console.log('CPU si spara');
+                        let cpuShot = loader.shift();
+                        if (cpuShot === 0) {
+                            console.log('Si spara un blank');
+                            alert('CPU si spara un blank. Turno del CPU.');
+                            cpuTurn = true;
+                        } else {
+                            console.log('Si spara un live');
+                            alert('CPU si spara un live. Preso.')
+                            cpuLife--;
+                            cpuTurn = false;
+                        }
+                    }
+                }
             } else {
                 alert('Hai sparato un live. -1 Vita al CPU.');
                 cpuLife--;
                 console.log('Live sparato al CPU' + loader);
+                if (cpuLife > 0) {
+                    let cpuTurn = true;
+                    while (cpuTurn === true && loader.length > 0 && playerLife > 0) {
+                        const cpuShotDirection = Math.round(Math.random());
+                        if (cpuShotDirection === 0) {
+                            console.log('CPU spara al player');
+                            let cpuShot = loader.shift();
+                            if (cpuShot === 0) {
+                                console.log('Blank, povero idiota.');
+                                alert('CPU ti spara un blank. Mancato.');
+                                cpuTurn = false;
+                            } else {
+                                console.log('Live, player preso');
+                                alert('CPU ti spara un live. Preso.')
+                                playerLife--;
+                                cpuTurn = false;
+                            }
+                        } else {
+                            console.log('CPU si spara');
+                            let cpuShot = loader.shift();
+                            if (cpuShot === 0) {
+                                console.log('Si spara un blank');
+                                alert('CPU si spara un blank. Turno del CPU.');
+                                cpuTurn = true;
+                            } else {
+                                console.log('Si spara un live');
+                                alert('CPU si spara un live. Preso.')
+                                cpuLife--;
+                                cpuTurn = false;
+                            }
+                        }
+                    }
+                }
             }
         } else {
             alert('Scrivi "me" per sparare a te stesso o "you" per sparare al CPU.')
         }
     }
+    if (playerLife <= 0) {
+        alert("GAME OVER. La CPU ti ha battuto.");
+    } else if (cpuLife <= 0) {
+        alert("VITTORIA! Hai eliminato la CPU.");
+    } else {
+        alert("Proiettili terminati. Pareggio per questo round!");
+    }
+}
 
 
-/*Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
 
-/*Buckshot Roulette
-Generare un "caricatore" con 2 colpi "live" e 3 colpi "blank" (o casuali da 1 a 3) in posizioni random.
-Dichiara al giocatore quanti colpi e di che tipo sono nel caricatore.
-Giocatore scrive "me" se vuole sparare se stesso, "you" se vuole sparare il prossimo.
-Se spari un blank a te stesso, hai un turno in più - Se spari un blank al CPU, turno prossimo.
-Se spari un live a te stesso, perdi una vita, turno prossimo. - Se spari un live al CPU, perde una vita, turno prossimo.
-Una vita ognuno. (Metti comunque win condition vita del CPU a 0 come win condition)
-*/
+
 
 
 
